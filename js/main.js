01f3fbfdf3640a0730fd2e20e71fca8de1a073dc
ZeroLiam <liselotr@gmail.com>
2018-04-25 21:37:56 +0200

    loadable content tested

---
+++ js/main.js
@@ -0,0 +1,6 @@
+console.log("main.js loaded");
+
+$(document).ready(function(){
+    $("#task1a").load("../components/task1.html #task-1a");
+    $("#task2a").load("../components/task2.html #task-2a");
+});
