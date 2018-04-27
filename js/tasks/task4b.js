12dec037643102d738d8a62388ee5f6b77b84933
ZeroLiam <liselotr@gmail.com>
2018-04-27 07:57:07 +0200

    Added and tested Task4B

--- js/tasks/task4b.js
+++ js/tasks/task4b.js
@@ -1,1 +1,33 @@
 console.log("load task 4b");
+var totalClicks = 0;
+var dblClicks = 0;
+var singleClicks = 0;
+
+//check if our div is present
+if($("#task_4b").length > 0){
+  $("#balloon").on('dblclick', registerDblClick);
+  $("#balloon").on('click', registerClick);
+}
+
+function registerDblClick(evt){
+  console.log($(this).outerHeight());
+  console.log($(this).outerWidth());
+
+  dblClicks++;
+  var increase = 50;
+  $(this).css("height", $(this).outerHeight() + increase + "px");
+  $(this).css("width", $(this).outerWidth() + increase + "px");
+  totalClicks++;
+
+  if($(this).outerHeight() >= 450){
+    $(this).hide();
+    console.log("totalClicks: " + totalClicks);
+    console.log("dblClicks: " + dblClicks);
+    console.log("singleClicks: " + singleClicks);
+  }
+}
+
+function registerClick(evt){
+  totalClicks++;
+  singleClicks++;
+}
