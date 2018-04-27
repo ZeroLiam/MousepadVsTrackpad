660ae5095c863520a82c4e5864c480da463291cb
ZeroLiam <liselotr@gmail.com>
2018-04-27 07:09:10 +0200

    Added navigation and text for tasks

--- js/main.js
+++ js/main.js
@@ -1,28 +1,18 @@
 console.log("main.js loaded");
-// var setA = ["1a", "2a", "3a", "4a", "5a"];
-// var setB = ["1b", "2b", "3b", "4b", "5b"];
-// var randomSet = [];
-//
-//     window.setTimeout(function(){
-//       // tasks.sort(function() { return 0.5 - Math.random() });
-//       setA.sort(function() { return 0.5 - Math.random() });
-//       setB.sort(function() { return 0.5 - Math.random() });
-//
-//       for(var i = 0; i <= 4; i++){
-//         var choseSet = Math.floor((Math.random() * 2) + 1);
-//
-//         if(choseSet == 1){
-//           randomSet[i] = setA[i];
-//           randomSet[i + 5] = setB[i];
-//         }else if(choseSet == 2){
-//           randomSet[i] = setA[i];
-//           randomSet[i + 5] = setB[i];
-//         }
-//       }
-//         console.log(randomSet);
-//     },1000);
 
-$(document).ready(function(){
+//hide all elements
+hideAllButtons();
+
+function hideAllButtons(){
+  //Hide elements
+  $('#nextSession').hide();
+  $('#endSession').hide();
+  $('#sessionA').hide();
+  $('#sessionB').hide();
+}
+
+//Shuffles and creates tasks
+function createTasks(evt){
   var setA = ["1a", "2a", "3a", "4a", "5a"];
   var setB = ["1b", "2b", "3b", "4b", "5b"];
   var randomSet = [];
@@ -45,16 +35,53 @@ $(document).ready(function(){
 
     window.setTimeout(function(){
       loadTasks(randomSet);
-    }, 2000);
+    }, 100);
+    window.setTimeout(function(){
+      $('#nextSession').show();
+    }, 1000);
+}
 
-    // $("#task1a").load("../components/task1.html #task-1a");
-    // $("#task2a").load("../components/task2.html #task-2a");
-});
-
+//Loads the task in its corresponding section
 function loadTasks(arr){
-  // console.log("#" + arr[0]);
-  // $("#task1" ).load("../components/task_1a.html #task_1a");
+  //scrolls the body to the task sections
+  $("html, body").animate({
+        scrollTop: $("header").outerHeight() - $("nav").outerHeight()
+    }, 1000);
+
+  //loads the tasks
   for(var ind = 0; ind < arr.length; ind++){
     $("#task" + (ind + 1)).load("../components/" + arr[ind] + ".html #" + arr[ind]);
   }
+    $('#sessionA').show();
+}
+
+//Loads next session of tasks
+function loadNextSession(evt){
+  $('#sessionA').hide();
+  $('#nextSession').hide();
+  $('#startTest').hide();
+
+    //scrolls the body to the task sections
+    $("html, body").animate({
+          scrollTop: $("header").outerHeight() - $("nav").outerHeight()
+      }, 1000);
+
+  $('#sessionB').show();
+  $('#endSession').show();
+}
+
+//Finishes the test
+function endSession(evt){
+  //hide all elements
+  hideAllButtons();
+  //scrolls the body to the task sections
+  $("html, body").animate({
+        scrollTop: 0
+  }, 1000);
+
+  endSessionMsg("Thank you for your cooperation!");
+}
+
+function endSessionMsg(msg){
+  $("#endMsg").html(msg);
 }
